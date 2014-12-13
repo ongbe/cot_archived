d29756c98cc9441b0fcbac17ed34db4e801f08d1
Paul Adamson <paul.adamson.01@gmail.com>
2014-12-13 13:18:53 -0700

    add wheat, corn, and soybean plots

--- cot.bash
+++ cot.bash
@@ -11,9 +11,12 @@ cat f_2014.txt f_2013.txt > f_all_year.csv
 rm f_201*.*
 sed 's/CRUDE OIL\, LIGHT SWEET/CRUDE OIL LIGHT SWEET/' f_all_year.csv > f_sub.csv
 awk -F "," '{OFS=","};{print $1, $3, $8, $22, $23 }' f_sub.csv > f_col.csv
-sed -n '/CRUDE OIL LIGHT SWEET - NEW YORK MERCANTILE EXCHANGE/p' f_col.csv > cot.csv
+sed -n '/CRUDE OIL LIGHT SWEET - NEW YORK MERCANTILE EXCHANGE/p' f_col.csv > cot_oil.csv
+sed -n '/CORN - CHICAGO BOARD OF TRADE/p' f_col.csv > cot_corn.csv
+sed -n '/WHEAT - CHICAGO BOARD OF TRADE/p' f_col.csv > cot_wheat.csv
+sed -n '/SOYBEAN MEAL - CHICAGO BOARD OF TRADE/p' f_col.csv > cot_beans.csv
 rm f_*.csv
 cd ..
 mkdir -p reports
 MathKernel -noprompt -run "<<./cot.m"
-rm scratch/cot.csv
+rm scratch/cot_*.csv
--- cot.m
+++ cot.m
@@ -1,22 +1,26 @@
-datafile=StringJoin[$InitialDirectory,"/scratch/cot.csv"];
-data=Import[datafile,"DateStringFormat"->{"Year","-","Month","-","Day"}];
-data2=Table[{data[[i,2]],100*N[(data[[i,4]]-data[[i,5]])/data[[i,3]]]},{i,Length[data]}];
-oildata=FinancialData["NYSE:OIL",data2[[Length[data2],1]]];
-oilplot=DateListPlot[oildata,
-  Joined->True, 
-  PlotStyle->Blue,
-  ImagePadding->50,
-  Frame->{True,True,True,False},
-  FrameStyle->{Automatic,Blue,Automatic,Automatic},
-  FrameLabel->{"Date","OIL","Confidence of Traders (CoT) and OIL ETF Price"},
-  ImageSize->500];
-cbotplot=DateListPlot[data2,Joined->True,Epilog->Line[{{data2[[1,1]],0},{data2[[Length[data2],1]],0}}],
-  PlotStyle->Red,ImagePadding->50,
-  Axes->False,Frame->{False,False,False,True},
-  FrameTicks->{None,None,None,All},
-  FrameStyle->{Automatic,Automatic,Automatic,Red},ImageSize->500,
-  FrameLabel->{"","","","CoT"}];
-date=DateString[{"Hour24","Minute",".","Second","-","MonthShort","-","Day","-","Year"}];
-filename=StringJoin[$InitialDirectory,"/reports/cot-",date,".pdf"];
-Export[filename,Overlay[{oilplot,cbotplot},ImageSize->500]];
-Exit[]
+commodities = {"oil", "corn", "wheat", "beans"};
+etfs = {"OIL", "CORN", "WEAT", "SOYB"};
+For[i=1,i<=4,i++,
+  datafile = StringJoin[$InitialDirectory, "/scratch/cot_", commodities[[i]], ".csv"];
+  data = Import[datafile, "DateStringFormat" -> {"Year", "-", "Month", "-", "Day"}];
+  data2 = Table[{data[[i, 2]], 100*N[(data[[i, 4]] - data[[i, 5]])/data[[i, 3]]]}, {i, Length[data]}];
+  etfdata = FinancialData[etfs[[i]], data2[[Length[data2], 1]]];
+  etfplot = DateListPlot[etfdata, Joined -> True, PlotStyle -> Blue, 
+    ImagePadding -> 50, Frame -> {True, True, True, False}, 
+    FrameStyle -> {Automatic, Blue, Automatic, Automatic}, 
+    FrameLabel -> {"Date", etfs[[i]],
+    StringJoin["Confidence of Traders (CoT) and ", etfs[[i]], " ETF Price"]}, ImageSize -> 500];
+  cbotplot = DateListPlot[data2, 
+    Joined -> True, 
+    Epilog -> Line[{{data2[[1, 1]], 0}, {data2[[Length[data2], 1]], 0}}], 
+    PlotStyle -> Red, ImagePadding -> 50, Axes -> False, 
+    Frame -> {False, False, False, True}, 
+    FrameTicks -> {None, None, None, All}, 
+    FrameStyle -> {Automatic, Automatic, Automatic, Red}, 
+    ImageSize -> 500, FrameLabel -> {"", "", "", "CoT"}];
+  date = DateString[{"Hour24", "Minute", ".", "Second", "-", "MonthShort", "-", "Day", "-", "Year"}];
+  filename = StringJoin[$InitialDirectory,"/reports/cot-", etfs[[i]], "-", date, ".pdf"];
+  Export[filename, Overlay[{etfplot, cbotplot}, ImageSize -> 500]]
+]
+Exit[];
+
